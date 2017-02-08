82b7b48a5875c1c03f56cc8a6f3453e55a9445d2
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2017-02-08 09:28:48 +0100

    Hello Lazy World o/

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const ncp = require('ncp');
+
+function fullproject(name) {
+  let dirname = name;
+  ncp(__dirname + '/boilerplate/', process.cwd() + '/' + name, function(err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(dirname + ' has been created! Happy frontend-ing!');
+  });
+
+
+}
+
+program
+  .version('0.0.1')
+  .usage('[options]')
+  .option('-c, --create <name>', 'Create a full boilerplate project', fullproject)
+  .parse(process.argv);
